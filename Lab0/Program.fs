790a6fce3f085c7f9efb8f96aea6064136fee8ed
Luna <luna@desroches.org>
2026-09-14 16:07:56 -0400

    Initial Commit, Lab0 Completed

---
+++ Lab0/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+module HelloSquare
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv =
+    printfn "%d squared is: %d!" 12 (square 12)
+    0
