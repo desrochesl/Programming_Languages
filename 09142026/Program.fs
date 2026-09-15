5154abb3c909671a831f111f0fa14277811c41ad
Luna <luna@desroches.org>
2026-09-15 11:33:12 -0400

    Add 09152026 assignment

---
+++ 09142026/Program.fs
@@ -0,0 +1,51 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+module HelloSquare
+
+let square x = x * x
+
+
+let equality_checker ()  = 
+    let x = 10
+    let y = 20
+
+    let result =
+        if (x < y) then "less"
+        elif (x = y) then "equal"
+        else "greater"
+
+    printfn "%s" result
+
+    0
+
+let temperature_checker (temp: int) =
+    let weatherStatus = 
+        if (temp <= 0) then "freezing"
+        else if (temp < 15) then "cold"
+        else if (temp < 35) then "warm"
+        else "hot"
+
+    printfn "It is %s today" weatherStatus
+
+let grade_checker (grade: int) = 
+    let gradeStatus = 
+        if (grade >= 90) then "A"
+        else if (grade >= 80) then "B"
+        else if (grade >= 70) then "C"
+        else if (grade >= 60) then "C"
+        else "Fail"
+
+    printfn "Your grade is: %s" gradeStatus 
+
+[<EntryPoint>]
+let main argv =
+    // 09-14-2026
+    printfn "%d squared is: %d!" 12 (square 12)
+
+    // 09-15-2026
+    equality_checker() |> ignore
+
+    temperature_checker(2) |> ignore
+
+    grade_checker(87) |> ignore
+
+    0
--- Lab0/Program.fs
+++
@@ -1,9 +0,0 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-module HelloSquare
-
-let square x = x * x
-
-[<EntryPoint>]
-let main argv =
-    printfn "%d squared is: %d!" 12 (square 12)
-    0
